596bcdec41eb6dabf2dd01c36e5d297012539526
jkindla <82672312+jkindla@users.noreply.github.com>
2022-11-05 14:41:29 -0500

    edit

---
+++ pictures/script.js
@@ -0,0 +1,75 @@
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+
+
+$(window).scroll(function() {
+    var header = $(document).scrollTop();
+    var headerHeight = $(".header").outerHeight();
+    var firstSection = $(".main-container section:nth-of-type(1)").outerHeight();
+    if (header > headerHeight) {
+      $(".header").addClass("fixed");
+    } else {
+      $(".header").removeClass("fixed");
+    }
+    if (header > firstSection) {
+      $(".header").addClass("in-view");
+    } else {
+      $(".header").removeClass("in-view");
+    }
+  });
+
+
+
+
+
+  
+
+
+
+
+
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
